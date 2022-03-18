06c5f1631428c281bc50bfcb2fc152a887d89e07
JaplinChen <53631327+JaplinChen@users.noreply.github.com>
2022-03-18 13:37:55 +0700

    修正 CASE 語法

--- _Source/5_CreateTableStructureSQL.py
+++ _Source/5_CreateTableStructureSQL.py
@@ -10,13 +10,14 @@ def jsonToDataFrame(jsonStr):
      return pd.json_normalize(jsonStr)
 
 #TableName_Columns = ['TableID', 'TableName', 'TableNameViet', 'ModuleID', 'ModuleName', 'ModuleType']
-#TableStructure_Columns=['TableID', 'TableName', 'ModuleID', 'sID', 'ID', '_FieldName', 'NameVietnam', 'Type', 'Length', 'Description']
+#TableStructure_Columns=['TableID', 'TableName', 'ModuleID', 'sID', 'ID', 'field_name', 'NameVietnam', 'Type', 'Length', 'Description']
 
 # -- SQL Strings for Table Structure
-TableSQL = '''--{TableName}
-SELECT
-{FieldString}
-FROM {TableID}
+table_sql = '''-- {TableName}
+-- Column = {FieldColumn}
+SELECT 
+{FieldString} 
+FROM {TableID} 
 '''
 
 def Description_Convert_to_SQL_CASE(Description):
@@ -25,12 +26,12 @@ def Description_Convert_to_SQL_CASE(Description):
         return ''
     # int_regex = r'/^[+-]?\d+$/'
     regex = re.compile(r'([+-]?\w)\.|([+-]?\w):')
-    replaceString = r"|   WHEN '\1\2' THEN N'"
+    replace_string = r"|   WHEN '\1\2' THEN N'"
     f = ''
     m = ''
     n = ''
     if re.match(regex,s):
-        f = regex.sub(replaceString,s)
+        f = regex.sub(replace_string,s)
         for x in f.split('|'):
             n = n + x.rstrip() + "'\n"
         m = n[2:]  #--去掉第一個 '\n
@@ -62,63 +63,74 @@ _TableName_df = jsonToDataFrame(_TableName)
 _TableStructure_df = jsonToDataFrame(_TableStructure)
 # --------------------------------------------------------------------
 # -- Table Structure : Ready to build one html file for each class
-last_Module_ID = ''
-Table_ModuleID = ''
+last_module_id = ''
+table_module_id = ''
 for x in _TableName_df.index:
+    # -- 判斷是否 剛開始
+    if (last_module_id) == '':
+        # -- 設定 table_module_id & last_module_id
+        table_module_id = str(_TableName_df.loc[x]['ModuleID']).strip()
+        last_module_id   = table_module_id
+    # -- 判斷是否 ModuleID 不同
+    if _TableName_df.loc[x]['ModuleID'].strip() != last_module_id:
+        # -- 檢查資料夾是否存在, 如果不存在，就建立資料夾
+        if not os.path.isdir('SQL'):       
+            os.makedirs('SQL', mode=0o777)
+        if not os.path.isdir('SQL\\'+table_module_id):
+            os.makedirs('SQL\\'+table_module_id, mode=0o777)
+        # -- 設定 table_module_id & last_module_id
+        table_module_id = str(_TableName_df.loc[x]['ModuleID']).strip()
+        last_module_id = table_module_id
 
-    if (last_Module_ID) == '':
-            # -- 設定 Table_ModuleID & last_Module_ID
-            Table_ModuleID = str(_TableName_df.loc[x]['ModuleID']).strip()
-            last_Module_ID = Table_ModuleID
-
-    if _TableName_df.loc[x]['ModuleID'].strip() != last_Module_ID:
-        if not os.path.isdir('SQL'):       # -- 檢查資料夾是否存在
-            os.makedirs('SQL', mode=0o777)     # -- 如果不存在，就建立資料夾
-        if not os.path.isdir('SQL\\'+Table_ModuleID):       # -- 檢查資料夾是否存在
-            os.makedirs('SQL\\'+Table_ModuleID, mode=0o777)     # -- 如果不存在，就建立資料夾
-        Table_ModuleID = _TableName_df.loc[x]['ModuleID'].strip()
-        last_Module_ID = Table_ModuleID
-
-    _TableID =  str(_TableName_df.loc[x]['TableID']).strip()
-    TableName = _TableID +'_'+ str(_TableName_df.loc[x]['TableName']).strip().replace('/','')
+    table_id =  str(_TableName_df.loc[x]['TableID']).strip()
+    table_name = table_id +'_'+ str(_TableName_df.loc[x]['TableName']).strip().replace('/','')
 
     # ----------------------------------------------------------------------------------------------------------------------
     # -- 取得 Table Structure 資料
     # -- 只需要 _TableStructure_df.loc[x]['TableID'] 的資料
-    _TableStructure_df1 = _TableStructure_df.loc[ _TableStructure_df['TableID'] == _TableID ]
+    _TableStructure_df1 = _TableStructure_df.loc[ _TableStructure_df['TableID'] == table_id ]
     # -- ['TableID', 'TableName', 'ModuleID', 'sID', 'ID', 'FieldName', 'NameVietnam', 'Type', 'Length', 'Description']
-    FieldString = ''
+    field_string = ''
+    field_name_column = []
     if len(_TableStructure_df1) != 0:
         y = 0
         for y in _TableStructure_df1.index:
-            _FieldID        = str(_TableStructure_df1.loc[y]['ID'])
-            _FieldName  = str(_TableStructure_df1.loc[y]['FieldName'])
-            _Description = str(_TableStructure_df1.loc[y]['Description'])
-            _DescriptionConvert = Description_Convert_to_SQL_CASE(_Description)
-            if _FieldName != '預留欄位' and _FieldName != '預留':
+            field_id        = str(_TableStructure_df1.loc[y]['ID'])
+            field_name  = str(_TableStructure_df1.loc[y]['FieldName'])
+            description = str(_TableStructure_df1.loc[y]['Description'])
+            description_convert = Description_Convert_to_SQL_CASE(description)
+            #print(field_name[0:2])
+            #if field_name != '預留欄位' and field_name != '預留'  and field_name[0:2] != '預留':
+            if field_name[0:2] != '預留' and field_name[0:2] != '特性'  and field_name[0:4] != '特殊特性'  and field_name[0:4] != '屬性內容'  and field_name[0:4] != '屬性代碼':
                 if y == 0:
-                    FieldString = '   ' + _FieldID + ' AS "' + _FieldName  + '"\n'
+                    field_string = '   ' + field_id + ' AS "' + field_name  + '"\n'
                 else:
-                    if  _DescriptionConvert != '':
-                        FieldString = FieldString + '  ,CASE \n' + _DescriptionConvert + '   ELSE ""\n   END AS "' + _FieldName + '"\n'
+                    if  description_convert != '':
+                        field_string = field_string + '  ,CASE ' + field_id + '\n' + description_convert + '   ELSE ""\n   END AS "' + field_name + '"\n'
                     else:
-                        FieldString = FieldString + '  ,' + _FieldID + ' AS "' + _FieldName + '"\n'
-        if FieldString.strip()[0:1] == ',':
-            FieldString = '   ' + FieldString[3:]
-            
-    if not os.path.isdir('SQL'):       # -- 檢查資料夾是否存在
-        os.makedirs('SQL', mode=0o777)      # -- 如果不存在，就建立資料夾
-    if not os.path.isdir('SQL\\'+Table_ModuleID):       # -- 檢查資料夾是否存在
-        os.makedirs('SQL\\'+Table_ModuleID, mode=0o777)     # -- 如果不存在，就建立資料夾
+                        field_string = field_string + '  ,' + field_id + ' AS "' + field_name + '"\n'
+                
+                field_name_column.append(field_name)
+                
+        #print(field_name_column)        
+        if field_string.strip()[0:1] == ',':
+            field_string = '   ' + field_string[3:]
+    
+    # -- 檢查資料夾是否存在, 如果不存在，就建立資料夾
+    if not os.path.isdir('SQL'):
+        os.makedirs('SQL', mode=0o777)
+    if not os.path.isdir('SQL\\'+table_module_id):
+        os.makedirs('SQL\\'+table_module_id, mode=0o777)
 
     # OUTPUT AN HTML FILE
-    with open('SQL\\'+Table_ModuleID+'\\'+TableName+'.sql', 'w',encoding="utf-8") as f:
-        SQLString = TableSQL.format(
-            FieldString=FieldString,
-            TableName=TableName,
-            TableID=_TableID
+    with open('SQL\\'+table_module_id+'\\'+table_name+'.sql', 'w',encoding="utf-8") as f:
+        SQL_String = table_sql.format(
+            FieldColumn = str(field_name_column),
+            FieldString = field_string,
+            TableName = table_name,
+            TableID = table_id
             )
-        f.write(SQLString)
+        f.write(SQL_String)
         f.close()
 
 
